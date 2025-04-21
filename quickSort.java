e342cbaf8b7be03089a4f83ff9506f35114387f9
Abhijith Shaju <abhijithshaju2004@gmail.com>
2025-04-21 20:56:25 +0530

    quickSort

--- quickSort.java
+++ quickSort.java
@@ -1,3 +1,15 @@
+/*
+1. Time Complexity
+Best Case	O(n log n)	Pivot always divides the array into two nearly equal parts
+Average Case	O(n log n)	Random pivot selection leads to balanced partitions
+Worst Case	O(n²)	Pivot is always the smallest/largest element (unbalanced partitions)
+
+2. Space Complexity
+Best/Average Case	O(log n)	Recursion stack depth (balanced partitions)
+Worst Case	O(n)	Recursion stack depth (unbalanced partitions)
+*/
+
+
 public class quickSort {
     public static int partition(int[] arr, int low, int high) {
         int pivot = arr[high];
@@ -36,7 +48,7 @@ public class quickSort {
     public static void main(String[] args) {
         int[] arr = {1, 3, 4, 2, 5, 6, 9, 7};
         int len = arr.length;
-        
+
         QuickSort(arr, 0, len-1);
         print(arr);
     }
